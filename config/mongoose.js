10f84d5be53c5edb194e7c7093760acab14c72c7
Subhash Chandra Verma <104697404+subhash202122@users.noreply.github.com>
2023-10-09 12:22:43 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 async function main(){
-    await mongoose.connect('mongodb://127.0.0.1:27017/');
+    await mongoose.connect('mongodb+srv://subhashchandrav10:QZX4IogdgHeVnijG@cluster0.qelaizv.mongodb.net/?retryWrites=true&w=majority
+');
     console.log("connection Successfull !! ");
 }
 main().catch(error =>console.log("connection not successfull !!"));
