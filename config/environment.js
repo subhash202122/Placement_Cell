c462f0e1852a2b4c675a9f40a7570ab11e1021e7
Subhash Chandra Verma <104697404+subhash202122@users.noreply.github.com>
2023-10-15 15:14:24 +0530

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ const developement = {
     customMware_path : './config/flashMessage',
     assets_path : './assets',
     secret_key: 'Csub6EetXLBAqRbmqTnJmqPim4B+hWgh1eSCFd2jyJk=',
-    mongoose_path: 'mongodb://127.0.0.1:27017/',
+    mongoose_path: 'mongodb+srv://subhashchandrav10:QZX4IogdgHeVnijG@cluster0.qelaizv.mongodb.net/?retryWrites=true&w=majority',
     api_path :'https://remotive.com/api/remote-jobs'
 }
 module.exports = developement;
