ff4481157ef20e019642806cc263d4f3f17665e3
Subhash Chandra Verma <104697404+subhash202122@users.noreply.github.com>
2023-10-15 15:52:00 +0530

    Update student.js

--- controller/student.js
+++ controller/student.js
@@ -85,16 +85,110 @@
 //         fs.unlinkSync('./studentData.csv');
 //     });
 // }
+// const Company = require("../models/company");
+// const Student = require("../models/student");
+
+// module.exports.home = async function (req, res) {
+//   const data = await Student.find({});
+//   return res.render("home", {
+//     title: "Home Page",
+//     student_list: data,
+//   });
+// };
+// module.exports.studentForm = function (req, res) {
+//   return res.render("create_student", {
+//     title: "Create Student",
+//   });
+// };
+
+// module.exports.interviewInfo = function (req, res) {
+//   return res.render("interview_info", {
+//     title: "Interviews Info",
+//   });
+// };
+
+// module.exports.createStudent = async function (req, res) {
+//   const newStudent = await Student.create(req.body);
+//   res.redirect("/");
+// };
+
+// module.exports.deleteStudent = async function (req, res) {
+//   const id = req.query.id;
+
+//   await Company.updateMany(
+//     {},
+//     {
+//       $pull: {
+//         students: { student: id },
+//       },
+//     },
+//     { multi: true }
+//   );
+//   // const companies = await Company.find({});
+
+//   // for (let company of companies) {
+//   //   if (company && company.students.length > 0) {
+//   //     const filteredStudents = company.students.filter(
+//   //       (student) => student.student != id
+//   //     );
+//   //     company.students = filteredStudents;
+//   //     company.save();
+//   //   }
+//   // }
+//   await Student.findByIdAndDelete(id);
+//   res.redirect("/");
+// };
+
+// module.exports.studentInfo = async function (req, res) {
+//   const id = req.query.id;
+//   const data = await Student.findById(id);
+
+//   if (data) {
+//     return res.render("student_info", {
+//       title: "Student Info",
+//       studentData: data,
+//     });
+//   }
+//   return res.redirect("back");
+// };
+
+// module.exports.updateStudent = async function (req, res) {
+//   const {
+//     id,
+//     name,
+//     batch,
+//     college,
+//     status,
+//     dsa_score,
+//     webdev_score,
+//     react_score,
+//   } = req.body;
+//   await Student.findByIdAndUpdate(id, {
+//     name,
+//     batch,
+//     college,
+//     status,
+//     dsa_score,
+//     webdev_score,
+//     react_score,
+//   });
+//   return res.redirect("/");
+// };
 const Company = require("../models/company");
 const Student = require("../models/student");
 
 module.exports.home = async function (req, res) {
-  const data = await Student.find({});
-  return res.render("home", {
-    title: "Home Page",
-    student_list: data,
-  });
+  try {
+    const data = await Student.find({});
+    return res.render("home", {
+      title: "Home Page",
+      student_list: data,
+    });
+  } catch (error) {
+    return res.send('Error in fetching student data');
+  }
 };
+
 module.exports.studentForm = function (req, res) {
   return res.render("create_student", {
     title: "Create Student",
@@ -108,69 +202,75 @@ module.exports.interviewInfo = function (req, res) {
 };
 
 module.exports.createStudent = async function (req, res) {
-  const newStudent = await Student.create(req.body);
-  res.redirect("/");
+  try {
+    const newStudent = await Student.create(req.body);
+    return res.redirect("/");
+  } catch (error) {
+    return res.send('Error in creating student');
+  }
 };
 
 module.exports.deleteStudent = async function (req, res) {
-  const id = req.query.id;
+  try {
+    const id = req.query.id;
 
-  await Company.updateMany(
-    {},
-    {
-      $pull: {
-        students: { student: id },
+    await Company.updateMany(
+      {},
+      {
+        $pull: {
+          students: { student: id },
+        },
       },
-    },
-    { multi: true }
-  );
-  // const companies = await Company.find({});
+      { multi: true }
+    );
 
-  // for (let company of companies) {
-  //   if (company && company.students.length > 0) {
-  //     const filteredStudents = company.students.filter(
-  //       (student) => student.student != id
-  //     );
-  //     company.students = filteredStudents;
-  //     company.save();
-  //   }
-  // }
-  await Student.findByIdAndDelete(id);
-  res.redirect("/");
+    await Student.findByIdAndDelete(id);
+    return res.redirect("/");
+  } catch (error) {
+    return res.send('Error in deleting student');
+  }
 };
 
 module.exports.studentInfo = async function (req, res) {
-  const id = req.query.id;
-  const data = await Student.findById(id);
+  try {
+    const id = req.query.id;
+    const data = await Student.findById(id);
 
-  if (data) {
-    return res.render("student_info", {
-      title: "Student Info",
-      studentData: data,
-    });
+    if (data) {
+      return res.render("student_info", {
+        title: "Student Info",
+        studentData: data,
+      });
+    }
+    return res.redirect("back");
+  } catch (error) {
+    return res.send('Error in fetching student info');
   }
-  return res.redirect("back");
 };
 
 module.exports.updateStudent = async function (req, res) {
-  const {
-    id,
-    name,
-    batch,
-    college,
-    status,
-    dsa_score,
-    webdev_score,
-    react_score,
-  } = req.body;
-  await Student.findByIdAndUpdate(id, {
-    name,
-    batch,
-    college,
-    status,
-    dsa_score,
-    webdev_score,
-    react_score,
-  });
-  return res.redirect("/");
+  try {
+    const {
+      id,
+      name,
+      batch,
+      college,
+      status,
+      dsa_score,
+      webdev_score,
+      react_score,
+    } = req.body;
+    await Student.findByIdAndUpdate(id, {
+      name,
+      batch,
+      college,
+      status,
+      dsa_score,
+      webdev_score,
+      react_score,
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.send('Error in updating student');
+  }
 };
